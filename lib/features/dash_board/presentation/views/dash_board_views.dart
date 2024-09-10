be8fb34e2cc7feadd4338600ad4626b31a1bf782
AQ <cshazemelwazery@gmail.com>
2024-09-10 17:49:13 +0300

    add web layout and custom drawer

--- lib/features/dash_board/presentation/views/dash_board_views.dart
+++ lib/features/dash_board/presentation/views/dash_board_views.dart
@@ -1,4 +1,5 @@
 import 'package:english_learning_platform/features/dash_board/presentation/views/widgets/adaptive_layout.dart';
+import 'package:english_learning_platform/features/dash_board/presentation/views/widgets/web_layout_view.dart';
 import 'package:flutter/material.dart';
 
 class DashBoardViews extends StatelessWidget {
@@ -7,9 +8,12 @@ class DashBoardViews extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
         body: AdaptiveLayout(
             mobileLayout: (context) => const Text("data"),
             tabletLayout: (context) => const Text("dsd"),
-            webLayout: (context) => const Text("dsvgfs")));
+            webLayout: (context) => const WebLayoutView()));
   }
 }
+
+
---
+++ lib/features/dash_board/presentation/views/widgets/web_layout_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'custom_drawer.dart';
+
+class WebLayoutView extends StatelessWidget {
+  const WebLayoutView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomDrawer();
+  }
+}
