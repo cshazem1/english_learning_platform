a6961fb91dd237d53bff3e3ffef2a43830a8b620
AQ <cshazemelwazery@gmail.com>
2024-09-10 17:18:48 +0300

    add assets images

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,12 @@
+class Assets {
+  static const String svgAvatar = 'assets/images/svg/avatar.svg';
+  static const String svgBook = 'assets/images/svg/book.svg';
+  static const String svgBook2 = 'assets/images/svg/book2.svg';
+  static const String svgBookNewOne2 = 'assets/images/svg/book_new_one_2.svg';
+  static const String svgFolder = 'assets/images/svg/folder.svg';
+  static const String svgFrame = 'assets/images/svg/Frame.svg';
+  static const String svgFrame2 = 'assets/images/svg/Frame2.svg';
+  static const String svgFrame3 = 'assets/images/svg/Frame3.svg';
+  static const String svgRocketDynamicColor = 'assets/images/svg/rocket-dynamic-color.svg';
+  static const String svgSubtract = 'assets/images/svg/Subtract.svg';
+}
